65088f561aa652af76b9ed1d9ff9e653aa669187
waltersblake <blake.walters@icloud.com>
2021-12-04 19:08:19 +1100

    Move timer state up to workout_timer

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,118 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:workout_timer/workout_timer.dart';
 
 typedef TimerCallback = void Function();
 
-class Timer extends StatefulWidget {
-  const Timer({Key? key, required this.data}) : super(key: key);
+class TimerDisplay extends StatelessWidget {
+  const TimerDisplay({Key? key, required this.data, required this.timerLabel,
+    required this.buttonLabel}) : super(key: key);
 
-  final TimerData data;
-
-  @override
-  State<Timer> createState() => _TimerState();
-}
-
-class _TimerState extends State<Timer> with SingleTickerProviderStateMixin {
-  late final Ticker _ticker;
-  late Duration _setTime;
-  late Duration _restTime;
-  late Duration _countDown;
-  final Duration _second = const Duration(seconds: 1);
-  String _toggleButtonLabel = 'Start';
-  TimerCallback? _reset;
-  var _count = 0;
-  var _isSet = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _setTime = widget.data.workout;
-    _restTime = widget.data.rest;
-    _countDown = _setTime;
-    _ticker = createTicker(_tick);
-  }
-
-  @override
-  void dispose() {
-    _ticker.dispose();
-    super.dispose();
-  }
-
-  void _tick(Duration elapsed){
-    // ticker ticks with the screen rate, 60fps.
-    // since we only care about seconds, set the state once every 60 frames.
-    if(_count == 60){
-      _count = 0;
-      // switch displayed timer between the set time and the rest time,
-      // and update completed sets if setTime is finishing
-      if(_countDown == Duration.zero){
-        if(_isSet){
-          widget.data.onSetsUpdate();
-        }
-        _toggleSetRest();
-        return;
-      }
-      // count down one second
-      setState(() {
-        _countDown = _countDown - _second;
-      });
-    } else {
-      _count++;
-    }
-
-  }
-
-  void _toggleSetRest(){
-    if(_isSet){
-      setState(() {
-        _countDown = _restTime;
-      });
-    } else {
-      setState(() {
-        _countDown = _setTime;
-      });
-    }
-    _isSet = !_isSet;
-  }
-
-  void _toggleTicker() {
-    if(_ticker.isActive){
-      _ticker.stop();
-      setState(() {
-        _toggleButtonLabel = 'Start';
-      });
-    } else {
-      _ticker.start();
-      setState(() {
-        _toggleButtonLabel = 'Pause';
-        _reset = _resetTicker;
-      });
-    }
-  }
-
-  void _resetTicker() {
-    if (_ticker.isActive) {
-      _ticker.stop();
-    }
-    setState(() {
-      _countDown = _isSet ? widget.data.workout : widget.data.rest;
-      _toggleButtonLabel = 'Start';
-      _reset = null;
-    });
-  }
+  final TimerCallbacks data;
+  final String timerLabel;
+  final String buttonLabel;
 
   @override
   Widget build(BuildContext context) {
     return Padding(padding: const EdgeInsets.all(16),
-    child: Column(
-      children: [
-        Text('$_countDown'),
-        const SizedBox(width: 0,
-        height: 90,),
-        TimerButtons(onToggle: _toggleTicker, onReset: _reset, label: _toggleButtonLabel),
-      ],
-    ));
+        child: Column(
+          children: [
+            Text(timerLabel),
+            const SizedBox(width: 0,
+              height: 90,),
+            TimerButtons(onToggle: data.toggleTicker, onReset: data.reset, label: buttonLabel),
+          ],
+        ));
   }
 }
 
@@ -136,13 +45,9 @@ class TimerButtons extends StatelessWidget {
   }
 }
 
-class TimerData {
-  TimerData({required this.workout, required this.rest, required this.totalSets,
-    required this.totalCycles, required this.onCyclesUpdate, required this.onSetsUpdate});
-  Duration workout;
-  Duration rest;
-  int totalSets;
-  int totalCycles;
-  SetsCallback onSetsUpdate;
-  CyclesCallback onCyclesUpdate;
+class TimerCallbacks {
+  TimerCallbacks({required this.toggleTicker, required this.reset});
+  TickerCallback toggleTicker;
+  ResetCallback? reset;
+
 }
